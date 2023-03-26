6ba373d2a38e61ae299bdf4891b025bbb64bd835
Banee Ishaque K <baneeishaque@gmail.com>
2023-03-26 19:33:54 +0530

    typo

---
+++ blog_spider.py
@@ -0,0 +1,15 @@
+# TODO : Create Scrapy Run Configuration Template
+
+import scrapy
+
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['https://www.zyte.com/blog/']
+
+    def parse(self, response, **kwargs):
+        for title in response.css('.oxy-post-title'):
+            yield {'title': title.css('::text').get()}
+
+        for next_page in response.css('a.next'):
+            yield response.follow(next_page, self.parse)
